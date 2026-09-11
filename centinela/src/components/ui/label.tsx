3074fe6947b762971f23e58b37bf28bc4cb76a2a
Cristian <cristian.mg.david@gmail.com>
2026-09-11 18:39:01 -0300

    refactor(label): simplificar estilo

--- centinela/src/components/ui/label.tsx
+++ centinela/src/components/ui/label.tsx
@@ -1,12 +1,13 @@
 "use client"
 
 import * as React from "react"
+import { cn } from "cn"
 
 function Label({ className, ...props }: React.ComponentProps<"label">) {
   return (
     <label
       data-slot="label"
-      className="text-label"
+      className={cn("text-label", className)}
       {...props}
     />
   )
