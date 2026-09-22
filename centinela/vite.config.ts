3112eea38e11586033c640c864fa1a2d87a395db
Belinda <i_belinda_y@hotmail.com>
2026-09-21 21:25:01 -0300

    se cambian extensiones

--- centinela/vite.config.ts
+++ centinela/vite.config.ts
@@ -9,11 +9,18 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  
+
   // Add the following configuration to resolve the alias
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+      },
+    },
+  },
 })
